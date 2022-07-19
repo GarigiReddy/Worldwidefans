07c75d633df889ce3fbafb9ec8dc44c8e53a5c2e
hp <hp@DESKTOP-MCTOCK3>
2022-07-19 22:08:08 +0530

    fans club association

---
+++ prabhas/src/test/java/Contact/CreatecontactTest.java
@@ -0,0 +1,14 @@
+package Contact;
+
+import org.testng.annotations.Test;
+
+public class CreatecontactTest {
+@Test
+public void CreateonlyTest() {
+	System.out.println("create contact for die hard fans");
+}
+@Test
+public void deleteTest() {
+	System.out.println("delete fake numbers");
+}
+}
---
+++ prabhas/src/test/java/Contact/modifyTest.java
@@ -0,0 +1,10 @@
+package Contact;
+
+import org.testng.annotations.Test;
+
+public class modifyTest {
+@Test
+public void updatecontactTEST() {
+	System.out.println("update fans number");
+}
+}
